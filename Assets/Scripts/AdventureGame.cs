f8fab3bc62ff8b004b0cdf4a1b673f4166523ba9
Tarık Köprülü <tarikk3545@gmail.com>
2018-10-14 14:27:57 +0300

    Ufonun Tutsakları v1 git commit -m Ufonun Tutsakları v1

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    [SerializeField] Text pathText;
+
+    State currentState;
+	// Use this for initialization
+	void Start () {
+        currentState = startingState;
+        textComponent.text = currentState.GetStoryState();
+        pathText.text = currentState.GetPath();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    void ManageState()
+    {
+        State[] nextStates = currentState.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1)&& nextStates.Length>=1)
+        {
+            currentState = nextStates[0];
+            textComponent.text = currentState.GetStoryState();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2)&& nextStates.Length>=2)
+        {
+            currentState = nextStates[1];
+            textComponent.text = currentState.GetStoryState();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3) && nextStates.Length==3)
+        {
+            currentState = nextStates[2];
+            textComponent.text = currentState.GetStoryState();
+        }
+        pathText.text = currentState.GetPath();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="State")] 
+public class State : ScriptableObject {
+
+  [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State nextPaths;
+    [SerializeField] State[] nextStates;
+    [TextArea(10, 3)] [SerializeField] string pathText;
+
+    public string GetStoryState()
+    {
+        return storyText;
+    }
+    public string GetPath()
+    {
+        return pathText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
